9f9ce5e54ced6de5090f70c4960050c03d709ac9
xxxswh <aa.danielyan0909@mail.ru>
2025-05-27 11:38:02 +0300

    kafka work performed

--- src/repository/mod.rs
+++ src/repository/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt::Error;
+
 pub mod trader;
 pub(crate) mod merchant;
 #[macro_export]
@@ -7,22 +9,27 @@ macro_rules! retry {
         let mut attempt = 0;
         loop {
             attempt += 1;
-            result = tokio::time::timeout(Duration::from_millis(300), $sql_func).await
+            result = tokio::time::timeout(Duration::from_millis(300), $sql_func).await;
             match result {
+                Ok(Ok(r)) => {
+                    break Ok(r)
+                },
                 Ok(Err(ref e)) if attempt < $max_retries && is_connection_err(e) => {
                     warn!(err=e.to_string(), "Error do request. Retrying...");
                     tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
                     continue;
                 },
-                Err(_) => {
+                Ok(Err(e)) => break Err(e.to_string()),
+                Err(e)if attempt < $max_retries => {
                     warn!(err=e.to_string(), "timeout do request. Retrying...");
+                    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+                    continue;
                 }
-               _ => {
-                    break;
+                Err(e) => {
+                    break Err(e.to_string());
                 }
             }
         }
-        result
     }};
 }
 pub fn is_connection_err<T>(e: &T) -> bool
